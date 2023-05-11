c0c973b90d89f32c2ca0879deb2ee35970b95304
NataliiaAndreieva <taliandreeva@gmail.com>
2023-05-11 18:02:00 +0300

    Update renderMarkup.js
    add markup

--- src/js/renderMarkup.js
+++ src/js/renderMarkup.js
@@ -1,4 +1,25 @@
 export default function renderBookCardsMarkup(books) {
-  const markup = null; // Тут получаем books, мапимся и рисуем разметку
+  const markup = books
+    .map(
+      ({ _id, book_image, title, author }) => `
+  <li data-book-id='${_id}'>
+  <a href="#">
+  <div>
+    <img src="${book_image}" alt="book" loading="lazy">
+  </div>
+  <div>
+    <div>
+      <p>"${title}"</p>
+    </div>
+    <div>
+      <p>"${author}"</p>
+    </div>
+  </div>
+  </a>
+</li>`
+    )
+    .join(''); 
   return markup;
 }
+
+
