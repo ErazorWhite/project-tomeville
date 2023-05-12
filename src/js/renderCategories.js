561cbce028bbd9b4034fee91545c91486354c733
mashanstrk <117375259+mashanstrk@users.noreply.github.com>
2023-05-13 00:34:09 +0300

    Створено список категорій та скрол

---
+++ src/js/renderCategories.js
@@ -0,0 +1,29 @@
+import { BookAPI } from './BookAPI';
+
+// Функція для отримання та відображення категорій книг
+async function displayBookCategories() {
+  const bookAPI = new BookAPI();
+  const categoriesList = document.querySelector('.categories-list');
+
+  try {
+    // Отримання списку об'єктів категорій книг з BookAPI
+    const response = await bookAPI.getCategoryList();
+    const categories = response.map(item => item.list_name);
+
+    // Сортування категорій в алфавітному порядку
+    categories.sort();
+
+    // Створення розмітки категорій
+    const markup = categories
+      .map(category => `<li class="categories-item">${category}</li>`)
+      .join('');
+
+    // Додавання розмітки до <ul> з початковим елементом
+    categoriesList.innerHTML = `<li class="categories-item">Усі категорії</li>${markup}`;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Виклик функції для відображення категорій книг
+displayBookCategories();
