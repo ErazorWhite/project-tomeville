fa93d9dddc761e8e7a7dd33409565ddd8e6a7397
Vasyl Lepish <117004345+AlessioItaliano@users.noreply.github.com>
2023-05-13 14:38:59 +0300

    finish

--- src/js/styleSwitcher.js
+++ src/js/styleSwitcher.js
@@ -1,10 +1,8 @@
 const htmlBody = document.querySelector('body');
 const swiperBtn = document.querySelector('#slider');
-// const logoIcon = document.getElementById('header__logo-icon');
-// console.log(logoIcon);
-// console.dir(logoIcon.href);
 const ligthLogo = document.querySelector('.header__logo-icon-light');
 const darkLogo = document.querySelector('.header__logo-icon-dark');
+
 invokeStyle();
 
 swiperBtn.addEventListener('change', toggleStyle);
@@ -13,21 +11,13 @@ function toggleStyle() {
   if (localStorage.getItem('style') === 'dark-style') {
     saveStyle('light-style');
     swiperBtn.setAttribute('aria-label', 'Світлий стиль сайту');
-    darkLogo.classList.remove('logo-active');
-    ligthLogo.classList.add('logo-active');
-    // logoIcon.setAttribute(
-    //   'href',
-    //   '/src/images/symbol-defs.svg#white-theme-logo'
-    // );
+    darkLogo.classList.remove('is-active');
+    ligthLogo.classList.add('is-active');
   } else {
     saveStyle('dark-style');
     swiperBtn.setAttribute('aria-label', 'Темний стиль сайту');
-    ligthLogo.classList.remove('logo-active');
-    darkLogo.classList.add('logo-active');
-    // logoIcon.setAttribute(
-    //   'href',
-    //   '/src/images/symbol-defs.svg#black-them-logo'
-    // );
+    ligthLogo.classList.remove('is-active');
+    darkLogo.classList.add('is-active');
   }
 }
 
@@ -35,17 +25,14 @@ function invokeStyle() {
   if (localStorage.getItem('style') === 'dark-style') {
     saveStyle('dark-style');
     swiperBtn.checked = false;
+    ligthLogo.classList.remove('is-active');
+    darkLogo.classList.add('is-active');
   } else {
     saveStyle('light-style');
     swiperBtn.checked = true;
     swiperBtn.setAttribute('aria-label', 'Світлий стиль сайту');
-    ligthLogo.classList.remove('logo-active');
-    darkLogo.classList.add('logo-active');
-
-    // logoIcon.setAttribute(
-    //   'href',
-    //   '/src/images/symbol-defs.svg#white-them-logo'
-    // );
+    darkLogo.classList.remove('is-active');
+    ligthLogo.classList.add('is-active');
   }
 }
 
