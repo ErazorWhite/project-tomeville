46b6d309b7cd689c9e4d53de4e30dc95d54c900b
Vasyl Lepish <117004345+AlessioItaliano@users.noreply.github.com>
2023-05-13 10:00:40 +0300

    some changes on switch logo

--- src/js/styleSwitcher.js
+++ src/js/styleSwitcher.js
@@ -3,7 +3,8 @@ const swiperBtn = document.querySelector('#slider');
 // const logoIcon = document.getElementById('header__logo-icon');
 // console.log(logoIcon);
 // console.dir(logoIcon.href);
-
+const ligthLogo = document.querySelector('.header__logo-icon-light');
+const darkLogo = document.querySelector('.header__logo-icon-dark');
 invokeStyle();
 
 swiperBtn.addEventListener('change', toggleStyle);
@@ -12,6 +13,8 @@ function toggleStyle() {
   if (localStorage.getItem('style') === 'dark-style') {
     saveStyle('light-style');
     swiperBtn.setAttribute('aria-label', 'Світлий стиль сайту');
+    darkLogo.classList.remove('logo-active');
+    ligthLogo.classList.add('logo-active');
     // logoIcon.setAttribute(
     //   'href',
     //   '/src/images/symbol-defs.svg#white-theme-logo'
@@ -19,6 +22,8 @@ function toggleStyle() {
   } else {
     saveStyle('dark-style');
     swiperBtn.setAttribute('aria-label', 'Темний стиль сайту');
+    ligthLogo.classList.remove('logo-active');
+    darkLogo.classList.add('logo-active');
     // logoIcon.setAttribute(
     //   'href',
     //   '/src/images/symbol-defs.svg#black-them-logo'
@@ -34,6 +39,9 @@ function invokeStyle() {
     saveStyle('light-style');
     swiperBtn.checked = true;
     swiperBtn.setAttribute('aria-label', 'Світлий стиль сайту');
+    ligthLogo.classList.remove('logo-active');
+    darkLogo.classList.add('logo-active');
+
     // logoIcon.setAttribute(
     //   'href',
     //   '/src/images/symbol-defs.svg#white-them-logo'
