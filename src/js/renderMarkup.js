2cacba9d5a97be8cf79585fea6e47b169ca4dcf1
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-05-15 01:17:18 +0300

    hover/focus added for book cards

--- src/js/renderMarkup.js
+++ src/js/renderMarkup.js
@@ -34,17 +34,24 @@ export function renderBsBookCardsMarkup(bestBooksList) {
             .map(
               ({ _id, list_name, author, book_image, title }) => `
               <li class="bs-books__item data-id="${_id}">
-                <p class="bs-books__category">${list_name}</p>
-                <div class="bs-books__thumb">
-                    <img src="${book_image}" alt="book cover: ${title}" class="bs-books__picture" width="335" height="485">
-                </div>
-                <div class="bs-books__box">
-                  <h3 class="bs-books__title">${title}</h3>
-                  <p class="bs-books__author">${author}</p>
-                </div>
-                <div class="bs-books__button-box">
-                  <button class="bs-books__see-more-btn" type="button" data-role="see-more-btn" data-list="${list_name}">see more</button>
-                </div>
+              <p class="bs-books__category">${list_name}</p>
+              <a class="bs-books__link" href="#">
+                  <div class="bs-books__thumb">
+                      <img src="${
+                        book_image || 'src/images/noimage/noImage-desk@2x.png'
+                      }" alt="book cover: ${
+                title || 'No title'
+              }"class="bs-books__picture" width="335" height="485">
+                      <p class="bs-books__view">quick view</p>
+                  </div>
+                  <div class="bs-books__box">
+                  <h3 class="bs-books__title">${title || 'No title'}</h3>
+                  <p class="bs-books__author">${author || 'No author'}</p>
+                  </div>
+                  </a>
+                  <div class="bs-books__button-box">
+                    <button class="bs-books__see-more-btn" type="button" data-role="see-more-btn" data-list="${list_name}">see more</button>
+                  </div>
               </li>`
             )
             .join('')}
