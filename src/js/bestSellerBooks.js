9df899053011f60317407763d0e79b484ba38a0c
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-05-14 23:28:33 +0300

    no markup creates with JS, branch is working

---
+++ src/js/bestSellerBooks.js
@@ -0,0 +1,24 @@
+import { BookAPI } from './bookAPI';
+import { renderBsBookCardsMarkup } from './renderMarkup';
+import { Notify } from 'notiflix';
+
+function renderBestSellerBooks() {
+  const api = new BookAPI();
+
+  const bestSellersBooksList = document.querySelector(
+    '.bs-books__categories-list'
+  );
+
+  api.getTopBooks().then(resp => {
+    if (resp.length == 0) {
+      Notify.failure('There are no books, sorry.');
+      return;
+    }
+    renderBsBookCardsMarkup(resp);
+    bestSellersBooksList
+      .insertAdjacentHTML('beforeend', renderBsBookCardsMarkup(resp))
+      .catch(err => Notify.failure(err));
+  });
+}
+
+renderBestSellerBooks();
--- src/js/renderMarkup.js
+++ src/js/renderMarkup.js
@@ -1,4 +1,3 @@
-
 export default function renderBookCardsMarkup(books) {
   const markup = books
     .map(
@@ -24,3 +23,35 @@ export default function renderBookCardsMarkup(books) {
   return markup;
 }
 
+export function renderBsBookCardsMarkup(bestBooksList) {
+  bestBooksList.map(({ books }) => console.log(books));
+  const markup = bestBooksList
+    .map(
+      ({ list_name, books }) => `
+      <li class="bs-books__categories-item">
+        <h2 class="bs-books__category-title">${list_name}</h2>
+        <ul class="bs-books__list">
+          ${books
+            .map(
+              ({ _id, list_name, author, book_image, title }) => `
+              <li class="bs-books__item data-id="${_id}">
+                <p class="bs-books__category">${list_name}</p>
+                <div class="bs-books__thumb">
+                    <img src="${book_image}" alt="book cover: ${title}" class="bs-books__picture" width="335" height="485">
+                </div>
+                <div class="bs-books__box">
+                  <h3 class="bs-books__title">${title}</h3>
+                  <p class="bs-books__author">${author}</p>
+                </div>
+                <div class="bs-books__button-box">
+                  <button class="bs-books__see-more-btn" type="button" data-role="see-more-btn" data-list="${list_name}">see more</button>
+                </div>
+              </li>`
+            )
+            .join('')}
+        </ul>
+      </li>`
+    )
+    .join('');
+  return markup;
+}
