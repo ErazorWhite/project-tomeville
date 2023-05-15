93ff7c44cdfbe5ad55c5bd2373dddda27f6c4d9f
Molksy <mikefromenclave@gmail.com>
2023-05-15 14:24:26 +0300

    fix slider
    + npm slider
    + benefitsFounds.JS
    + benefitsFounds.html

--- src/js/benefitFounds.js
+++ src/js/benefitFounds.js
@@ -1,6 +1,23 @@
-let menuElem = document.getElementById('button');
-    let titleElem = menuElem.querySelector('.title');
+import Swiper from 'swiper';
 
-    titleElem.onclick = function() {
-      menuElem.classList.toggle('open');
-    };
+const sliderContainer = document.querySelector('.swiper-wrapper');
+
+const swiper = new Swiper(sliderContainer, {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next', 
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 20
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 40
+    }
+  }
+});
