3a5c4ceec42154fbae25216d3de7846be391ebe2
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-05-15 15:20:30 +0000

    Deploying to gh-pages from @ ErazorWhite/project-tomeville@6ab5f27452dcd04600962ebc33b544b9ef9c60c9 🚀

---
+++ index.d2d1e7a1.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var t=window.location.pathname;document.querySelectorAll(".header__btn").forEach((function(e){"Home"===e.textContent.trim()&&"/"===t||"Shopping list"===e.textContent.trim()&&"/shopping_list.html"===t?e.classList.add("active"):e.classList.remove("active")}))}));
+//# sourceMappingURL=index.d2d1e7a1.js.map
--- index.f44d5624.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",(function(){var t=window.location.pathname;document.querySelectorAll(".header__btn").forEach((function(e){"Home"===e.textContent.trim()&&"/index.html"===t||"Shopping list"===e.textContent.trim()&&"/shoppingList.html"===t?e.classList.add("active"):e.classList.remove("active")}))}));
-//# sourceMappingURL=index.f44d5624.js.map
