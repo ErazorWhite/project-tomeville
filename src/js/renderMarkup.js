4ed225e88b7a1223bb0651e494c60f60054b729d
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-05-16 22:55:33 +0300

    var --cubic added

--- src/js/renderMarkup.js
+++ src/js/renderMarkup.js
@@ -1,30 +1,5 @@
 import noImg from '../images/noImage/noImage-desk@1x.png';
 
-export default function renderBookCardsMarkup(books) {
-  const markup = books
-    .map(
-      ({ _id, book_image, title, author }) => `
-  <li class="book-card" data-book-id='${_id}'>
-  <a class="book-card__link" href="#">
-  <div class="book-card__thumb">
-    <img class="book-card__image" src="${book_image}" alt="${author} ${title}" loading="lazy">
-  <p class="book-card__notification">quick view</p>
-    </div>
-  <div>
-    <div>
-      <p class="book-card__title">"${title}"</p>
-    </div>
-    <div>
-      <p>"${author}"</p>
-    </div>
-  </div>
-  </a>
-</li>`
-    )
-    .join('');
-  return markup;
-}
-
 export function renderBsBookCardsMarkup(bestBooksList) {
   const markup = bestBooksList
     .map(
