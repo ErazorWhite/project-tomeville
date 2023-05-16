ecbea2d7e297c2d98d2d37058a7d6b4ae9a091e1
Serhii Khomenchuk <serhiikhomenchuk@gmail.com>
2023-05-16 20:41:56 +0300

    shopping list first push

--- src/js/shoppingList.js
+++ src/js/shoppingList.js
@@ -1,11 +1,14 @@
 import { BookAPI } from './bookAPI';
+import svgTrash from '../images/icons.svg';
+
 const shoppingList = document.querySelector(".basketList");
 const emptyShoppingList = document.querySelector(".emptyBasket");
-import svgTrash from '../images/icons.svg';
+
+
 
 const LOCALSTORAGE_KEY = "booksInShopingList";
 let booksId = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
-console.log(booksId);
+console.log(booksId.id.length);
 const api = new BookAPI();
 
 booksId.id.map(async (id) => {
@@ -15,7 +18,7 @@ booksId.id.map(async (id) => {
     api.id = id
     const response = await api.getBooksById();
      console.log(response);
-    
+    emptyShoppingList.innerHTML = "";
          
     shoppingList.insertAdjacentHTML('beforeend',createShoppingCardMarkup(response));
       
@@ -23,40 +26,9 @@ booksId.id.map(async (id) => {
     console.log(error.message);
   }
 })
-// console.log(booking);
-// booking.id = '643282b1e85766588626a0ba'
-//  BOOK_ID = e.target.closest('.book-card').dataset.id;
-//     api.id = BOOK_ID;
-//     const { book_image, title, author, description, buy_links } =
-//       await api.getBooksById();
 
-//  const fetchBooksById = async () => {
-//    try {
-   
-//     const response = await booking.getBooksById();
-//      console.log(response);
-//     // booksArr = response[0].books;
-         
-//     // shoppingList.innerHTML = createShoppingCardMarkup(booksArr);
-      
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// };
-
-// fetchBooksById();
-
-  
-// LOCALSTORAGE_KEY="";
-// let booksArr = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
 
 function createShoppingCardMarkup({author, book_image, description, list_name, title, _id,buy_links}) {
-  if (!localStorage.getItem(LOCALSTORAGE_KEY)) {
-  
-    return `<div class = "emptyShoppingList"><p class = "emptyText">This page is empty, add some books and proceed to order.</p>
-    <div class="thumbEmptyShoppingList"></div></div>
-            `
-  }
   
   
   return  ` <li>
@@ -77,7 +49,7 @@ function createShoppingCardMarkup({author, book_image, description, list_name, t
                   <h2 class="title">${title}</h2>
                   <p class="dump">Hardcover fiction</p>
                   <p class="description">
-                    ${description}
+                    ${description || 'No description'}
                   </p>
                   
                     <p class="author underscription">${author}</p>
@@ -109,9 +81,4 @@ function createShoppingCardMarkup({author, book_image, description, list_name, t
     
   
 }
-  //   input.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).email || "";
-  //   textArea.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).message || "";
-  // }
- 
-// }
-  // createShoppingCardMarkup();
+  
