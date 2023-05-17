6fdab04f3159e718930c9ce3ad6e0ee0af53a94f
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-05-17 18:35:01 +0300

    merged with main + a bunch of fixes

--- src/js/shoppingList.js
+++ src/js/shoppingList.js
@@ -1,84 +1,89 @@
 import { BookAPI } from './bookAPI';
 import svgTrash from '../images/icons.svg';
 import noImg from '../images/noImage/noImage-desk@1x.png';
-import storage from './localStorage';
-// import {BOOKS_IDS} from "./popup"
+import { spinerStart, spinerStop } from './spinner';
 
-const shoppingList = document.querySelector(".basketList");
-const emptyShoppingList = document.querySelector(".emptyBasket");
-shoppingList.addEventListener("click", onClick);
+const shoppingList = document.querySelector('.basketList');
+const emptyShoppingList = document.querySelector('.emptyBasket');
+shoppingList.addEventListener('click', onClick);
 let data;
-const LOCALSTORAGE_KEY = "booksInShopingList";
+const LOCALSTORAGE_KEY = 'booksInShopingList';
 
+spinerStart();
 
 let booksId = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
 console.log(booksId.id);
 
 function onClick(evt) {
   console.log(evt.target);
-  if (evt.target.nodeName !== "BUTTON" && evt.target.nodeName !== "svg" && evt.target.nodeName !== "use" ) {
+  if (
+    evt.target.nodeName !== 'BUTTON' &&
+    evt.target.nodeName !== 'svg' &&
+    evt.target.nodeName !== 'use'
+  ) {
     return;
   }
   const li = evt.target.closest('li');
   const dataId = li.dataset.id;
-  // console.log(dataId);
-  
+
   const idToRemove = booksId.id.indexOf(dataId);
   console.log(idToRemove);
   if (idToRemove !== -1) {
-    booksId.id.splice(idToRemove, 1)
+    booksId.id.splice(idToRemove, 1);
     console.log(booksId.id);
-    
-const updatedbBoksIdToString = JSON.stringify({
-  id: booksId.id,
-});
+
+    const updatedbBoksIdToString = JSON.stringify({
+      id: booksId.id,
+    });
 
     localStorage.setItem(LOCALSTORAGE_KEY, updatedbBoksIdToString);
-    
+
     console.log(booksId.id.length);
-   if (!booksId.id.length) {
-    emptyShoppingList.innerHTML = `<div class="emptyShoppingList">
+    if (!booksId.id.length) {
+      emptyShoppingList.innerHTML = `<div class="emptyShoppingList">
         <p class="emptyText">
           This page is empty, add some books and proceed to order.
         </p>
         <div class="thumbEmptyShoppingList"></div>
-      </div>`
-   }
-    // emptyShoppingList.innerHTML = "";
-    shoppingList.innerHTML = "";
-      rendering();
-    
+      </div>`;
+      emptyShoppingList.style.display = '';
+    }
+    shoppingList.innerHTML = '';
+    rendering();
   }
 }
-
 
 function rendering() {
   const api = new BookAPI();
-booksId.id.map(async (id) => {
-  try {
+  booksId.id.map(async id => {
+    try {
+      api.id = id;
+      const response = await api.getBooksById();
+      data = response;
+      emptyShoppingList.innerHTML = '';
+      emptyShoppingList.style.display = 'none';
 
-    // console.log(id);
-    api.id = id
-    const response = await api.getBooksById();
-    data = response
-    //  console.log(response);
-    emptyShoppingList.innerHTML = "";
-         
-    shoppingList.insertAdjacentHTML('beforeend', createShoppingCardMarkup(response));
-    // /
-  
-    // onClick()
-  } catch (error) {
-    console.log(error.message);
-  }
-})
+      shoppingList.insertAdjacentHTML(
+        'beforeend',
+        createShoppingCardMarkup(response)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
 }
 rendering();
+spinerStop();
 
-function createShoppingCardMarkup({author, book_image, description,title, _id,buy_links}) {
-  
-  
-  return  ` <li data-id="${_id}">
+function createShoppingCardMarkup({
+  author,
+  book_image,
+  description,
+  title,
+  _id,
+  buy_links,
+}) {
+  return ` <li data-id="${_id}">
               <article class="basketCard">
                 <button class="trashButton" type="button">
                   <svg width="20px" height="20px">
@@ -99,7 +104,9 @@ function createShoppingCardMarkup({author, book_image, description,title, _id,bu
                     ${description || 'No description'}
                   </p>
                   
-                    <p class="author underscription">${author || 'No author'}</p>
+                    <p class="author underscription">${
+                      author || 'No author'
+                    }</p>
                     <ul class="basketBuyLink">
                       <li>
                         <a
@@ -124,8 +131,5 @@ function createShoppingCardMarkup({author, book_image, description,title, _id,bu
                   
                 </div>
               </article>
-            </li>`
-    
-  
+            </li>`;
 }
-  
--- src/shopping_list.js
+++ src/shopping_list.js
@@ -1,26 +1,8 @@
 import './js/scroll-up';
 import './js/styleSwitcher';
 import './js/mobileMenu';
-
 import './js/shoppingList';
 import './js/makeHeaderTabsActive';
-
 import './js/support/supportSlider';
 import './js/support/charityFoundsRender';
-
-// import './js/bestSellerBooks';
-// import './js/popup';
-// import './js/scroll-up';
-// import './js/styleSwitcher';
-// import './js/mobileMenu';
-// import './js/renderCategories';
-// import './js/renderBooksByCategory';
-
 import './js/bookAPI';
-// import './js/createMarkupModal';
-// import './js/getDataFromMarkup';
-// import './js/localStorage';
-
-// import './js/shoppingList';
-
-// import './js/makeHeaderTabsActive';
